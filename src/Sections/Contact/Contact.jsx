9503682aaf3ed7d5ea3bb42d17aee336fbc9c34a
questcoderull <questcoderull@gmail.com>
2025-08-15 01:00:24 +0600

    update contact section

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-2 gap-8">
         {/* Left Side - Form */}
         <motion.div
-          className="bg-[#1e293b] p-6 rounded-xl shadow-lg py-12"
+          className="bg-[#1e293b] p-6 rounded-xl shadow-2xl py-12"
           initial={{ opacity: 0, x: -50 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6 }}
@@ -17,14 +17,14 @@ const Contact = () => {
         >
           <h2 className="text-2xl font-semibold mb-4">Send me a message</h2>
           <form className="flex flex-col gap-4">
-            <label>Your Name</label>
+            <label className="-mb-3">Your Name</label>
             <input
               type="text"
               required
               placeholder="Your full name"
               className="input input-bordered w-full bg-[#334155] py-6 border-gray-600 rounded-lg"
             />
-            <label>Your Email</label>
+            <label className="-mb-3">Your Email</label>
             <input
               type="email"
               placeholder="your@email.com"
@@ -32,7 +32,7 @@ const Contact = () => {
               className="input input-bordered w-full bg-[#334155] py-6 border-gray-600 text-white rounded-lg"
             />
 
-            <label>Your Message</label>
+            <label className="-mb-3">Your Message</label>
             <textarea
               placeholder="Tell me about your project or just say hello..."
               rows="4"
@@ -59,7 +59,7 @@ const Contact = () => {
           </p>
 
           <div className="space-y-4">
-            <div className="flex items-center gap-3 bg-[#334155] py-6 p-4 rounded-xl shadow">
+            <div className="flex items-center gap-3 bg-[#334155] py-4 p-4 rounded-xl shadow">
               <div className="bg-[#1E2F53] p-3 rounded-full">
                 <MdEmail className="text-blue-400 " size={24} />
               </div>
@@ -70,22 +70,26 @@ const Contact = () => {
                 </p>
               </div>
             </div>
-            <div className="flex items-center gap-3 bg-[#334155] py-6 p-4 rounded-xl shadow">
+            <div className="flex items-center gap-3 bg-[#334155] py-4 p-4 rounded-xl shadow">
               <div className="bg-[#1E2F53] p-3 rounded-full">
                 <MdPhone className="text-blue-400" size={24} />
               </div>
               <div>
                 <p className="text-lg font-medium">Phone</p>
-                <span>+8801314-961102</span>
+                <span className="hover:text-primary cursor-pointer">
+                  +8801314-961102
+                </span>
               </div>
             </div>
-            <div className="flex items-center gap-3 bg-[#334155] py-6 p-4 rounded-xl shadow">
+            <div className="flex items-center gap-3 bg-[#334155] py-4 p-4 rounded-xl shadow">
               <div className="bg-[#1E2F53] p-3 rounded-full">
                 <MdLocationOn className="text-blue-400" size={24} />
               </div>
               <div>
                 <p className="text-lg font-medium">Location</p>
-                <span>Sylhet, Bangladesh</span>
+                <span className="hover:text-primary cursor-pointer">
+                  Sylhet, Bangladesh
+                </span>
               </div>
             </div>
           </div>
